8592ac66d0b739edfb0d783d7096add0a7d3070d
Alexander Valenzuela <axvalenzuela@gmail.com>
2022-10-08 14:35:31 -0700

    Created function save_csv and updated CLI.

--- qualifier/utils/fileio.py
+++ qualifier/utils/fileio.py
@@ -28,3 +28,23 @@ def load_csv(csvpath):
         for row in csvreader:
             data.append(row)
     return data
+
+def save_csv(csvpath,data):
+    """Reads the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The csv file path.
+
+    Returns:
+        A list of lists that contains the rows of data from the CSV file.
+
+    """
+    with open(csvpath, "w", newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=",")
+
+        # Skip the CSV Header
+        #csvwriter.writerow(header)
+
+        # Write/Save the CSV data
+        for row in data:
+            csvwriter.writerow(row)
